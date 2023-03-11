ca35d7ecb87e8b35d6a966533b07acf0824c87e4
Irina Vorontsova <irina.n.vorontsova@gmail.com>
2023-03-11 15:19:31 +0300

    Final variant

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,4 +1,5 @@
 from datetime import time
+import pytest
 
 def test_dark_theme():
     """
@@ -36,20 +37,14 @@ def test_find_suitable_user():
         {"name": "Maria", "age": 18},
     ]
     # TODO найдите пользователя с именем "Olga"
-    suiable_user = []
 
-    for user in users:
-        if user['name'] == 'Olga':
-            suiable_user.append(user)
+    suiable_user = [user for user in users if user['name'] == 'Olga']
     suiable_user = suiable_user[0]
 
     assert suiable_user == {"name": "Olga", "age": 45}
     # TODO найдите всех пользователей младше 20 лет
-    suiable_users = []
 
-    for user in users:
-        if user['age'] <= 18:
-            suiable_users.append(user)
+    suiable_users = [user for user in users if user['age'] < 20]
 
     assert suiable_users == [
         {"name": "Stanislav", "age": 15},
@@ -66,19 +61,22 @@ def test_find_suitable_user():
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
 
+def change_function_name(function, **kwargs):
+    return f'''{function.__name__.replace('_', ' ').title()} {str(list(kwargs.values())).replace("'", "")}'''
+
 def test_readable_function():
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = change_function_name(open_browser, browser_name=browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = change_function_name(go_to_companyname_homepage, page_url=page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = change_function_name(find_registration_button_on_login_page, page_url=page_url, button_text=button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
